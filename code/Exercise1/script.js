8c7daeea2c0693d81b02314134ea913712f2dd5b
unknown <kikko_timov@yahoo.com  git config --global user.name>
2023-12-05 17:37:05 +0100

    test

---
+++ code/Exercise1/script.js
@@ -0,0 +1,75 @@
+
+// Exercise 1
+
+let price = 119.95;
+let quantity = 30;
+let tax = 5;
+let sum = price * quantity ;
+let Taxsum = (tax / 100) * sum;
+let Totalsum = sum + Taxsum;
+console.log('Total price:',  Totalsum);
+
+console.log('_______________________________________________________________')
+
+// Exercise 2
+
+
+let seconds = 60;
+let minutes = 5;
+const convert = minutes * 60;
+console.log('Minutes to seconds:', convert);
+
+console.log('_______________________________________________________________')
+
+
+// Exercise 3
+
+
+
+let myNumber = 1;
+myNumber++;
+console.log('Increment:', myNumber);
+
+console.log('_______________________________________________________________')
+
+
+// Exercise 4
+
+
+
+let base = 3;
+let height = 5;
+let area = (base * height) / 2;
+console.log('Triangle area:', area);
+
+console.log('_______________________________________________________________')
+
+// Exercise 5
+
+
+
+let length = 8;
+let width = 5;
+let area1 = length * width;
+let perimeter = 2 * (length * width);
+console.log('Area:', area1);
+console.log('Perimeter:', perimeter);
+
+console.log('_______________________________________________________________')
+
+
+//Exercise 6
+
+const pi = 3.14159;
+const radius = 10;
+const area2 = pi * radius * radius;
+console.log('Area of the my circle:',  area2);
+
+
+
+
+
+
+
+
+
