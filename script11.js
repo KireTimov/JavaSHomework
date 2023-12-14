3bd3e3d69772e28a128f6c45949b5e398f230466
unknown <kikko_timov@yahoo.com  git config --global user.name>
2023-12-14 13:28:56 +0100

    Homework 4 popravka

--- script11.js
+++
@@ -1,8 +0,0 @@
-function tellStory(array){
-    let i=0;
-    console.log(`This is ${array[i]}, ${array[i]} is a nice person but today he is ${array[i+1]} because he is ${array[i+2]} all day.`);
-}
-
-let storyArray = ["Ivan", "sad", "doing his hoework"];
-let story =tellStory(storyArray);
-console.log(" ");
