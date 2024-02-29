aebe7a33e892cd5a7b0b1feabbe4eeb6c38fe7fe
unknown <kikko_timov@yahoo.com  git config --global user.name>
2024-02-29 14:55:41 +0100

    Homework 01 Basic NodeJS

---
+++ NodeJS-Homework/index.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+
+const filePath = path.join(import.meta.dirname, 'homework.txt');
+fs.writeFile(filePath, 'Homework 01 in Basic NodeJS', (err) => {
+    if (err){
+        console.log("Error", err);
+    }
+   
+    fs.appendFile(filePath, ' FINISHED', (err) => {
+      if (err){
+        console.log("Error", err);
+      }
+      
+  
+      fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+        if (err){
+            console.log("Error", err);
+
+        }
+        
+        console.log(data);
+      });
+    })
+});
+
+// import fs from 'fs';
+// import path from 'path';
+
+// const filePath = path.join(import.meta.dirname, 'homework.txt');
+
+// async function writeFileAsync() {
+//   try {
+//     await fs.promises.writeFile(filePath, 'Homework 01 in Basic NodeJS');
+//     await fs.promises.appendFile(filePath, ' FINISHED');
+//     const data = await fs.promises.readFile(filePath, { encoding: 'utf-8' });
+//     console.log(data);
+//   } catch (err) {
+//     console.log("Error", err);
+//   }
+// }
+
+// writeFileAsync();
